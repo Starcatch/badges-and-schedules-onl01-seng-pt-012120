c9698b0d48c560f24a69effbdb74091f4773b75e
Timur Mishiev <timurmishiev@gmail.com>
2020-01-28 22:36:04 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,12 @@
-# Write your code here.
+def badge_maker(name)
+  return "Hello, my name is #{name}."
+end 
+  
+  attendees = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
+  
+
+def batch_badge_creator(array)
+  array.collect { |name| "Hello, my name is " + name}
+end 
+
+batch_badge_creator(attendees)
